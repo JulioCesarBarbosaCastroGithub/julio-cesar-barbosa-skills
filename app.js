503df58f6b5379d9efd250e43d9588d53c91fef6
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2022-09-18 20:11:08 -0300

    sixth commit

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Home page');
-})
+    res.render('home');
+});
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000!'));
