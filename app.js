1b659f62a2335b3d556450447c01c178dbd64da2
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2022-09-23 00:20:43 -0300

    ninth commit

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -17,8 +21,24 @@ app.get('/myskills', (req, res) => {
     res.render('mySkills');
 });
 
+app.get('/myskills/javascript', (req,res) => {
+    res.render('javaScript');
+});
+
+app.get('/projetos', (req, res) => {
+    res.render('projetos');
+});
+
 app.get('/contato', (req, res) => {
     res.render('contato');
+});
+
+app.post('/formularioenviado', (req, res) => {
+    var nome = req.body.nome;
+    var email = req.body.email;
+    var cidade = req.body.cidade;
+    var texto = req.body.texto;
+    res.send("Informações enviadas com sucesso! Obrigado por interagir! ✌️");
 });
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000!'));
