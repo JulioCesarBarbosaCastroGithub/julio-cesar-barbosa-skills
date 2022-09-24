3366525488785eb3a93aa3d68982fd91055bc8f1
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2022-09-23 22:58:06 -0300

    tenth commit

--- app.js
+++ app.js
@@ -21,8 +21,60 @@ app.get('/myskills', (req, res) => {
     res.render('mySkills');
 });
 
+app.get('/myskills/web', (req,res) => {
+    res.render('web');
+});
+
+app.get('/myskills/terminal', (req,res) => {
+    res.render('terminal');
+});
+
+app.get('/myskills/git', (req,res) => {
+    res.render('git');
+});
+
+app.get('/myskills/github', (req,res) => {
+    res.render('github');
+});
+
+app.get('/myskills/metodologiasageis', (req,res) => {
+    res.render('metodologiasAgeis');
+});
+
+app.get('/myskills/html', (req,res) => {
+    res.render('html');
+});
+
+app.get('/myskills/css', (req,res) => {
+    res.render('css');
+});
+
 app.get('/myskills/javascript', (req,res) => {
     res.render('javaScript');
+});
+
+app.get('/myskills/nodejs', (req,res) => {
+    res.render('nodeJs');
+});
+
+app.get('/myskills/sqlmysql', (req,res) => {
+    res.render('sqlMysql');
+});
+
+app.get('/myskills/api', (req,res) => {
+    res.render('api');
+});
+
+app.get('/myskills/react', (req,res) => {
+    res.render('react');
+});
+
+app.get('/myskills/angularjs', (req, res) => {
+    res.render('angularJs');
+});
+
+app.get('/myskills/java', (req, res) => {
+    res.render('java');
 });
 
 app.get('/projetos', (req, res) => {
